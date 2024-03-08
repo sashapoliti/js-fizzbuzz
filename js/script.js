3d21dc7adf958b74644722cd6f296e4fb3c4ec86
sashapoliti <sas.politi@gmail.com>
2024-03-08 14:19:17 +0100

    init with little structure

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Scrivi un programma che stampi in console i numeri da 1 a 100,
+// ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”.
+// Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+
+// num % 3, num % 5,num % 3 && num %5
+
+const wrapper = document.createElement('div'); //create div
